e18deecb3d3bfd63c55d54faedf391909677e2ff
AdShEaTaKoRe <tomy.berman1@gmail.com>
2020-03-11 08:53:42 +0000

    changed relationship structure, added joiner for list and item models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+List.destroy_all
+Item.destroy_all
+ListItem.destroy_all
+
+# -------------------------------------------
+u = User.create(first_name: "test", last_name: "testy", username: "testys", password_digest: "1234")
+
+i1 = Item.create(name: "Apple")
+i2 = Item.create(name: "Banana")
+i3 = Item.create(name: "Carrot")
+i4 = Item.create(name: "Date")
+
+l = List.create(name: "my-list", user_id: 1)
+
+li = ListItem.create(list_id: 1, item_id: 1)
+li = ListItem.create(list_id: 1, item_id: 2)
+li = ListItem.create(list_id: 1, item_id: 3)
+li = ListItem.create(list_id: 1, item_id: 4)
+
+
+
